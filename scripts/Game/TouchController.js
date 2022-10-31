21284185b50fad71b0b960d0b7e17e675fee2428
pashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2022-10-31 20:40:54 +0530

    Edited Audio File.

--- scripts/Game/TouchController.js
+++ scripts/Game/TouchController.js
@@ -3,11 +3,11 @@ import { Vector2D } from "../Lib/Math/Vector2D.js";
 
 export const TOUCH_EVENT_TYPES = {
     "undefined": 0b1,
-    "up": 0b1 << 1,
+    "up": 0b1 << 1, // up and click have same effect . kind of alias
     "click": 0b1 << 1,
     "down": 0b1 << 2,
 }
-
+Object.freeze(TOUCH_EVENT_TYPES);
 export const TOUCH_INFORMATION = {
     position: new Vector2D(0, 0),
     eventType: TOUCH_EVENT_TYPES.undefined,
